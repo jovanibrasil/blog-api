3214966f96230e6ec15bbfe66bb308f3cf891985
Jovani Oliveira Brasil <jovanibrasil@gmail.com>
2020-07-05 13:27:24 -0300

    Add basic logging for database and requests

--- src/configs/custom-express.js
+++ src/configs/custom-express.js
@@ -1,8 +1,26 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-app.use(express.static("public"));
+const morgan = require('morgan');
+const logger = require('./logger')
+
+app.use(express.static('public'));
 app.use(express.json());
+
+app.use(cors({
+    origin: ['http://localhost:3001'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
+app.use(morgan('common', {
+    stream: {
+        write: (message) => {
+            logger.info(message);
+        }
+    }
+}));
 
 const auth = require('./security/sec')();
 
--- src/configs/database/db-config.dev.js
+++ src/configs/database/db-config.dev.js
@@ -1,9 +1,14 @@
+const logger = require('../logger');
+
 module.exports = {
     database: 'ntask',
     host: 'localhost',
     username: 'postgres',
     password: 'password',
     dialect: 'postgres',
+    logging: (sql) => {
+        logger.info(`[${new Date()}] ${sql}`);
+    },
     define: {
         timestamps: true, // define created_at and updated_at
         underscored: true
